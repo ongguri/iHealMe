e7982d8af332be906cd1cf123a3f2b7994f877eb
GiwoongBang <bgw6713@naver.com>
2023-04-24 20:59:37 +0900

    Initial commit

---
+++ src/main/java/com/project/ihealme/IHealMeApplication.java
@@ -0,0 +1,13 @@
+package com.project.ihealme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IHealMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IHealMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/ihealme/IHealMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ihealme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IHealMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
