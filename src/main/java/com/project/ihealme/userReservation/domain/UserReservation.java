f0129ab3ff5819ef60358ee6c1b0b7d209c93b8d
ongguri <rkdtjsdyd321@naver.com>
2023-04-27 23:11:51 +0900

    User Reservation view refactoring

--- src/main/java/com/project/ihealme/userReservation/domain/UserReservation.java
+++ src/main/java/com/project/ihealme/userReservation/domain/UserReservation.java
@@ -1,5 +1,8 @@
 package com.project.ihealme.userReservation.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +10,7 @@ import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter @Setter
 public class UserReservation {
 
     @Id
@@ -18,52 +22,4 @@ public class UserReservation {
     private String txList;
     private LocalDateTime rDate;
     private String status;
-
-    public Long getResNo() {
-        return resNo;
-    }
-
-    public void setResNo(Long resNo) {
-        this.resNo = resNo;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
-
-    public String getHptName() {
-        return hptName;
-    }
-
-    public void setHptName(String hptName) {
-        this.hptName = hptName;
-    }
-
-    public String getTxList() {
-        return txList;
-    }
-
-    public void setTxList(String txList) {
-        this.txList = txList;
-    }
-
-    public LocalDateTime getrDate() {
-        return rDate;
-    }
-
-    public void setrDate(LocalDateTime rDate) {
-        this.rDate = rDate;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
