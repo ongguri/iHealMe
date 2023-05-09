54f1b17d32d751ebe0c2e58ccac625293f6d089b
Haerang <harang1226@naver.com>
2023-05-09 15:38:41 +0900

    develop branch merge

--- src/main/java/com/project/ihealme/community/repository/CommentRepository.java
+++ src/main/java/com/project/ihealme/community/repository/CommentRepository.java
@@ -2,7 +2,7 @@ package com.project.ihealme.community.repository;
 
 import com.project.ihealme.community.domain.Comment;
 import com.project.ihealme.community.domain.Post;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
