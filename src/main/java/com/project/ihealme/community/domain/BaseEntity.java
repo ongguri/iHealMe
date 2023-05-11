308a82fe642bdeb8fc93ddfe291e936a7dbdbaca
ongguri <rkdtjsdyd321@naver.com>
2023-05-11 13:48:28 +0900

    remove @Setter in BaseEntity

--- src/main/java/com/project/ihealme/community/domain/BaseEntity.java
+++ src/main/java/com/project/ihealme/community/domain/BaseEntity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-@Getter @Setter
+@Getter
 public abstract class BaseEntity {
 
     @CreatedDate
