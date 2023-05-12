534fbff2d2e2513a727777517f1b95e0c0d95073
Hyebeen <hyebeen.dev@gmail.com>
2023-05-12 22:01:01 +0900

    MODIFY Community Post

---
+++ src/main/resources/import.sql
@@ -0,0 +1,23 @@
+/*INSERT INTO users (user_id, answer, birth_date, business_num, email, gender, hpt_address, hpt_name, hpt_phone_num, name, password,phone_num, question, user_role) VALUES (1, 'dd', '991231', null, 'longlee@daum.net', '남', null, null, null, '방기웅', '1234', '01012341234', '질문1','USER')
+
+commit;
+
+INSERT INTO HPTRECEPTION (RESNO, EMAIL, PNAME, TXLIST, CURRENTSTATUS) VALUES (HIBERNATE_SEQUENCE.NEXTVAL, 'sebom@hospital.co.kr', '이성구', '영유아검진', '진료완료');
+INSERT INTO HPTRECEPTION (RESNO, EMAIL, PNAME, TXLIST, CURRENTSTATUS) VALUES (HIBERNATE_SEQUENCE.NEXTVAL, 'sebom@hospital.co.kr', '홍은채', '예방접종', '진료 전');
+INSERT INTO HPTRECEPTION (RESNO, EMAIL, PNAME, TXLIST, CURRENTSTATUS) VALUES (HIBERNATE_SEQUENCE.NEXTVAL, 'sebom@hospital.co.kr', '안유진', '소아진료', '접수대기');
+commit;
+
+INSERT INTO userreservation(resno, email, name, list, regdate) VALUES (userreservation_no_seq.nextval, 'longlee@daum.net', '새롬소아청년과의원', '영유아 검진', sysdate);
+INSERT INTO userreservation(resno, email, name, list, currentStatus, regdate) VALUES (userreservation_no_seq.nextval, 'longlee@daum.net', '이지소아청년과의원', '영유아 검진', '진료완료', sysdate);
+INSERT INTO userreservation(resno, email, name, list, currentStatus, regdate) VALUES (userreservation_no_seq.nextval, 'longlee@daum.net', '새롬소아청년과의원', '영유아 검진', '진료완료', sysdate);
+INSERT INTO userreservation(resno, email, name, list, currentStatus, regdate) VALUES (userreservation_no_seq.nextval, 'longlee@daum.net', '새롬소아청년과의원', '영유아 검진', '진료완료', sysdate);
+INSERT INTO userreservation(resno, email, name, list, currentStatus, regdate) VALUES (userreservation_no_seq.nextval, 'longlee@daum.net', '이지소아청년과의원', '영유아 검진', '진료완료', sysdate);
+INSERT INTO userreservation(resno, email, name, list, currentStatus, regdate) VALUES (userreservation_no_seq.nextval, 'longlee@daum.net', '이지소아청년과의원', '영유아 검진', '진료완료', sysdate);
+INSERT INTO userreservation(resno, email, name, list, currentStatus, regdate) VALUES (userreservation_no_seq.nextval, 'longlee@daum.net', '새롬소아청년과의원', '영유아 검진', '진료완료', sysdate);
+INSERT INTO userreservation(resno, email, name, list, currentStatus, regdate) VALUES (userreservation_no_seq.nextval, 'longlee@daum.net', '새롬소아청년과의원', '영유아 검진', '진료완료', sysdate);
+INSERT INTO userreservation(resno, email, name, list, currentStatus, regdate) VALUES (userreservation_no_seq.nextval, 'longlee@daum.net', '이지소아청년과의원', '영유아 검진', '진료완료', sysdate);
+INSERT INTO userreservation(resno, email, name, list, currentStatus, regdate) VALUES (userreservation_no_seq.nextval, 'longlee@daum.net', '이지소아청년과의원', '영유아 검진', '진료완료', sysdate);
+INSERT INTO userreservation(resno, email, name, list, currentStatus, regdate) VALUES (userreservation_no_seq.nextval, 'longlee@daum.net', '이지소아청년과의원', '영유아 검진', '진료완료', sysdate);
+INSERT INTO userreservation(resno, email, name, list, currentStatus, regdate) VALUES (userreservation_no_seq.nextval, 'longlee@daum.net', '새롬소아청년과의원', '영유아 검진', '진료완료', sysdate);
+commit;
+*/
