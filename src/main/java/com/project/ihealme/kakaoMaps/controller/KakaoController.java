2c177d94088e702e82aa852641afb97ec863f764
GiwoongBang <bgw6713@naver.com>
2023-05-12 16:37:30 +0900

    JWT 서버 사이드 구현 완료

--- src/main/java/com/project/ihealme/kakaoMaps/controller/KakaoController.java
+++ src/main/java/com/project/ihealme/kakaoMaps/controller/KakaoController.java
@@ -1,13 +1,16 @@
 package com.project.ihealme.kakaoMaps.controller;
 
-import com.project.ihealme.kakaoMaps.dto.KakaoMapsDto;
 import com.project.ihealme.kakaoMaps.service.KakaoMapsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @Controller
 //@RestController
@@ -22,7 +25,14 @@ public class KakaoController {
     }*/
 
     @GetMapping("/")
-    public String maps() {
+    public String maps(HttpServletRequest request, HttpServletResponse response, RedirectAttributes redirectAttributes) {
+        Map<String, ?> inputFlashMap = RequestContextUtils.getInputFlashMap(request);
+
+        if (inputFlashMap != null) {
+            String token = (String) inputFlashMap.get("token");
+            response.addCookie(new Cookie("token", token));
+        }
+
         return "maps/kakaoMaps";
     }
 
--- src/main/java/com/project/ihealme/user/controller/UserController.java
+++ src/main/java/com/project/ihealme/user/controller/UserController.java
@@ -5,16 +5,13 @@ import com.project.ihealme.user.dto.UserRequest;
 import com.project.ihealme.user.jwt.JwtConfig;
 import com.project.ihealme.user.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Collections;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -46,27 +43,29 @@ public class UserController {
     @PostMapping(value = "/registeruser")
     public String registerUser(@RequestBody UserRequest userRequest) {
         usersService.registerUser(userRequest);
-        return "redirect:/users/login";
-    }
-
-    @GetMapping("/my")
-    public UserDTO findUser(Authentication authentication) {
-        if (authentication == null) {
-            throw new BadCredentialsException("회원 정보를 찾을 수 없습니다.");
-        }
-        return usersService.findUser(authentication.getName());
-    }
-
-    @GetMapping("/admin")
-    @PreAuthorize("isAuthenticated() and hasRole('ROLE_ADMIN')")
-    public List<UserDTO> findAllUser() {
-        return usersService.findAll();
+        return "redirect:/login";
     }
 
     @PostMapping("/login")
-    public String login(UserRequest userRequest) {
-        UserDTO users = usersService.findByEmailAndPassword(userRequest.getEmail(), userRequest.getPassword());
-        return jwtConfig.createToken(users.getEmail(), Collections.singletonList(users.getUserRole().getValue()));
+    public String login(@RequestBody UserDTO userDTO, RedirectAttributes redirectAttributes) {
+        UserDTO user = usersService.findByEmailAndPassword(userDTO.getEmail(), userDTO.getPassword());
+        String token = jwtConfig.createToken(user.getEmail(), Collections.singletonList(user.getUserRole().getValue()));
+        redirectAttributes.addFlashAttribute("token",token);
+        return "redirect:/";
     }
+
+//    @GetMapping("/my")
+//    public UserDTO findUser(Authentication authentication) {
+//        if (authentication == null) {
+//            throw new BadCredentialsException("회원 정보를 찾을 수 없습니다.");
+//        }
+//        return usersService.findUser(authentication.getName());
+//    }
+
+//    @GetMapping("/admin")
+//    @PreAuthorize("isAuthenticated() and hasRole('ROLE_ADMIN')")
+//    public List<UserDTO> findAllUser() {
+//        return usersService.findAll();
+//    }
 
 }
