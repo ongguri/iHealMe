1f1ba00a4110a4b32e69907915ac363691f8eb0e
DOBIN <devpark0810@gmail.com>
2023-05-12 18:24:51 +0900

    05.12 KakaoMap Logic/view 수정

--- src/main/java/com/project/ihealme/kakaoMaps/controller/KakaoController.java
+++ src/main/java/com/project/ihealme/kakaoMaps/controller/KakaoController.java
@@ -1,18 +1,8 @@
 package com.project.ihealme.kakaoMaps.controller;
 
-import com.project.ihealme.kakaoMaps.dto.KakaoMapsDto;
-import com.project.ihealme.kakaoMaps.entity.KakaoMapsEntity;
-import com.project.ihealme.kakaoMaps.service.KakaoMapsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
-import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
