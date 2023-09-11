985420eeb91d84e010139835d4978978e5a6cad4
ongguri <rkdtjsdyd321@naver.com>
2023-09-11 18:05:50 +0900

    유저 예약, 병원 접수의 Dto 클래스 생성

---
+++ src/main/java/com/project/ihealme/userReservation/dto/UserReservationDto.java
@@ -0,0 +1,45 @@
+package com.project.ihealme.userReservation.dto;
+
+import com.project.ihealme.userReservation.domain.UserReservation;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter @Getter
+public class UserReservationDto {
+
+    private Long resNo;
+    private String patientName;
+    private String txList;
+    private String currentStatus;
+    private LocalDateTime regDate;
+    private LocalDateTime updateRegDate;
+
+    protected UserReservationDto() {
+    }
+
+    private UserReservationDto(Long resNo, String patientName, String txList, String currentStatus, LocalDateTime regDate, LocalDateTime updateRegDate) {
+        this.resNo = resNo;
+        this.patientName = patientName;
+        this.txList = txList;
+        this.currentStatus = currentStatus;
+        this.regDate = regDate;
+        this.updateRegDate = updateRegDate;
+    }
+
+    public static UserReservationDto of(Long resNo, String patientName, String txList, String currentStatus, LocalDateTime regDate, LocalDateTime updateRegDate) {
+        return new UserReservationDto(resNo, patientName, txList, currentStatus, regDate, updateRegDate);
+    }
+
+    public static UserReservationDto from(UserReservation entity) {
+        return new UserReservationDto(
+                entity.getResNo(),
+                entity.getPatientName(),
+                entity.getTxList(),
+                entity.getCurrentStatus(),
+                entity.getRegDate(),
+                entity.getUpdateRegDate()
+        );
+    }
+}
